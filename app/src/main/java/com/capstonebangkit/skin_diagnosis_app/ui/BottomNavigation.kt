adc35368ad2e5bc9be00d23216077931f4b84597
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2022-05-19 14:51:09 +0700

    perbaikan layout

--- app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/BottomNavigation.kt
+++ app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/BottomNavigation.kt
@@ -8,22 +8,22 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.capstonebangkit.skin_diagnosis_app.R
-import com.capstonebangkit.skin_diagnosis_app.databinding.ActivityMainHomeBinding
+import com.capstonebangkit.skin_diagnosis_app.databinding.BottomNavigationMainBinding
 
 
 class BottomNavigation : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainHomeBinding
+    private lateinit var binding: BottomNavigationMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainHomeBinding.inflate(layoutInflater)
+        binding = BottomNavigationMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main_home)
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_nav)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
