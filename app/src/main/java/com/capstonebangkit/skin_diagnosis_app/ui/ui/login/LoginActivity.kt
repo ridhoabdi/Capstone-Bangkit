7dc97606ceedaea4567ad7bd187f74bb886b6dc1
irkhammuh11@gmail.com <irkhammuh11@gmail.com>
2022-05-27 00:10:08 +0700

    Login-Register-Firebase

--- app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/ui/login/LoginActivity.kt
@@ -1,14 +1,72 @@
 package com.capstonebangkit.skin_diagnosis_app.ui.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.capstonebangkit.skin_diagnosis_app.databinding.ActivityLoginBinding
+import com.capstonebangkit.skin_diagnosis_app.ui.MainActivity
+import com.capstonebangkit.skin_diagnosis_app.ui.ui.register.RegisterActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    lateinit var binding: ActivityLoginBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnLogin.setOnClickListener{
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty()) {
+                binding.emailEditText.error = "Email Harus diisi"
+                binding.emailEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.emailEditText.error = "Email tidak Valid"
+                binding.emailEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()){
+                binding.passwordEditText.error = "Password Harus diisi"
+                binding.passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 6){
+                binding.passwordEditText.error = "Password Minimal 6 Karakter"
+                binding.passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+            LoginFirebase(email,password)
+        }
+
+    }
+
+    private fun LoginFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Selamat Datang $email",  Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/ui/register/RegisterActivity.kt
@@ -1,14 +1,74 @@
 package com.capstonebangkit.skin_diagnosis_app.ui.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.capstonebangkit.skin_diagnosis_app.databinding.ActivityRegisterBinding
-
+import com.capstonebangkit.skin_diagnosis_app.ui.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityRegisterBinding
+
+    lateinit var binding: ActivityRegisterBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        binding.btnRegister.setOnClickListener{
+            val nama = binding.nameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty()) {
+                binding.emailEditText.error = "Email Harus diisi"
+                binding.emailEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.emailEditText.error = "Email tidak Valid"
+                binding.emailEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()){
+                binding.passwordEditText.error = "Password Harus diisi"
+                binding.passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 6){
+                binding.passwordEditText.error = "Password Minimal 6 Karakter"
+                binding.passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+            RegisterFirebase(nama,email,password)
+        }
+    }
+
+    private fun RegisterFirebase(nama: String, email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Register Berhasil",  Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
