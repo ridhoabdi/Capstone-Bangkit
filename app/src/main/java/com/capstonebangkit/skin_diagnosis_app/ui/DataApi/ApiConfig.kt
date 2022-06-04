1114349e5d2fa5a77b02107fff905a00dffe6fe6
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2022-06-04 07:33:07 +0700

    new update

--- app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DataApi/ApiConfig.kt
+++ app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DataApi/ApiConfig.kt
@@ -14,7 +14,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("http://34.101.114.32:5000/")
+                .baseUrl("http://192.168.0.117:5000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
