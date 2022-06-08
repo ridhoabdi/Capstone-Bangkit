c4f034a15c43ebd5a7e7261807bd7b25444a22b1
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2022-06-08 21:17:02 +0700

    new update

--- app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DashboardUI/Deteksi/ScanActivity.kt
+++ app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DashboardUI/Deteksi/ScanActivity.kt
@@ -1,16 +1,20 @@
 package com.capstonebangkit.skin_diagnosis_app.ui.DashboardUI.Deteksi
 
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.capstonebangkit.skin_diagnosis_app.R
 import com.capstonebangkit.skin_diagnosis_app.databinding.ActivityCameraBinding
 import com.capstonebangkit.skin_diagnosis_app.databinding.ActivityScanBinding
+import com.capstonebangkit.skin_diagnosis_app.ui.DashboardUI.safeDeteksi.ChickenpoxActivity
+import com.capstonebangkit.skin_diagnosis_app.ui.DashboardUI.safeDeteksi.ScabiesActivity
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
@@ -29,6 +33,8 @@ class ScanActivity : AppCompatActivity() {
         val tvPredict: TextView = findViewById(R.id.predict)
         val tvPrecent: TextView = findViewById(R.id.precent)
         val imgObat : ImageView = findViewById(R.id.imageObat)
+        val btn : Button = findViewById(R.id.safePredict)
+
 
         val hasilprediksi = intent.getStringExtra(EXTRA_pred)
         val hasilpresentase = intent.getStringExtra(EXTRA_prec)
@@ -41,6 +47,16 @@ class ScanActivity : AppCompatActivity() {
         tvPredict.text = text
         tvPrecent.text = text2
         imgObat.setImageBitmap(result)
+
+        btn.setOnClickListener {
+            if (text == "Scabies"){
+                val intent = Intent(this,ScabiesActivity::class.java)
+                startActivity(intent)
+            }else{
+                val intent = Intent(this,ChickenpoxActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
 
