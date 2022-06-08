3d0720f1765ce9db239cbbd6a914818e76e90f13
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2022-06-09 00:42:57 +0700

    new update tampilan

--- app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DashboardUI/safeDeteksi/ChickenpoxActivity.kt
+++ app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DashboardUI/safeDeteksi/ChickenpoxActivity.kt
@@ -2,11 +2,39 @@ package com.capstonebangkit.skin_diagnosis_app.ui.DashboardUI.safeDeteksi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.capstonebangkit.skin_diagnosis_app.R
+import java.io.IOException
+import java.io.InputStream
 
 class ChickenpoxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chickenpox)
+        val txtContext: TextView = findViewById(R.id.textMedical)
+
+
+        // A string variable to store the text from the text file
+        val myOutput: String
+
+        // Declaring an input stream to read data
+        val myInputStream: InputStream
+
+        // Try to open the text file, reads
+        // the data and stores it in the string
+        try {
+            myInputStream = assets.open("Chickenpox.txt")
+            val size: Int = myInputStream.available()
+            val buffer = ByteArray(size)
+            myInputStream.read(buffer)
+            myOutput = String(buffer)
+
+            // Sets the TextView with the string
+            txtContext.text = myOutput
+
+        } catch (e: IOException) {
+            // Exception
+            e.printStackTrace()
+        }
     }
 }
--- app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DashboardUI/safeDeteksi/ScabiesActivity.kt
+++ app/src/main/java/com/capstonebangkit/skin_diagnosis_app/ui/DashboardUI/safeDeteksi/ScabiesActivity.kt
@@ -1,12 +1,43 @@
 package com.capstonebangkit.skin_diagnosis_app.ui.DashboardUI.safeDeteksi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.TextView
 import com.capstonebangkit.skin_diagnosis_app.R
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
 
 class ScabiesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scabies)
+        val txtContext: TextView = findViewById(R.id.textMedical)
+
+
+        // A string variable to store the text from the text file
+        val myOutput: String
+
+        // Declaring an input stream to read data
+        val myInputStream: InputStream
+
+        // Try to open the text file, reads
+        // the data and stores it in the string
+        try {
+            myInputStream = assets.open("Scabies.txt")
+            val size: Int = myInputStream.available()
+            val buffer = ByteArray(size)
+            myInputStream.read(buffer)
+            myOutput = String(buffer)
+
+            // Sets the TextView with the string
+            txtContext.text = myOutput
+
+        } catch (e: IOException) {
+            // Exception
+            e.printStackTrace()
+        }
     }
 }
